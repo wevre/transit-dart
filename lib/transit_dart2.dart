bcc7f0952dc18612ec4a2ca6b0c1062474d5b4b3
wevre <6877948+wevre@users.noreply.github.com>
2023-03-11 21:49:11 -0700

    renames and grammar stuff

--- lib/transit_dart2.dart
+++ lib/transit_dart2.dart
@@ -1,7 +1,7 @@
 /// Support for doing something awesome.
 ///
 /// More dartdocs go here.
-library transit_dart;
+library transit_dart2;
 
 import 'package:fixed/fixed.dart';
 import 'package:uuid/uuid.dart' as yuli_uuid;
